67a048ab47e72429e292e4c66abd64453fa399ed
Magda <128220619+MagdalenaRosa@users.noreply.github.com>
2023-10-30 23:03:59 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,28 +7,21 @@ public class Main {
                 {4, 45, 6},
                 {7, 8, 9}
         };
-        int tab[]={1,2,3};
+        int [] tab={1,2,3};
 
         int suma = obliczSumeDW(tablica);
         System.out.println(max_value_tab_wielo(tablica));
         System.out.println(max_value_tab_jedno(tab));
-        zmniejszanie_licz();
+   
     }
-
-    public static void zmniejszanie_licz(){
-        for(int i=20;i>=10;i--){
-            System.out.println(i);
-        }
-
+    
     }
-   static int max_value_tab_jedno(int []tab){
-       int max = Arrays.stream(tab).max().getAsInt();
-       return max;
+    static int max_value_tab_jedno(int []tab){
+       return max = Arrays.stream(tab).max().getAsInt();
 
     }
     static int max_value_tab_wielo(int [][]tab_wielo){
-        int max =Arrays.stream(tab_wielo).flatMapToInt(Arrays::stream).max().getAsInt();
-    return max;
+        return max =Arrays.stream(tab_wielo).flatMapToInt(Arrays::stream).max().getAsInt();
     }
 
     public static int obliczSumeDW(int[][] tablicaLiczb) {
